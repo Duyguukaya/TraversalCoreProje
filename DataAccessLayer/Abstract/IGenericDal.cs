b5cfac5e648b3c571fb7b6aea9d813938bdd10e6
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-10-21 14:54:42 +0300

    dataaccess katmanı tamamlandı.

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T>
+    {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetList();
+    }
+}
