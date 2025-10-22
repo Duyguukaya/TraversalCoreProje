d0800e2aebd1e021ca03a5e04c9fa56f47543d99
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-10-22 14:31:33 +0300

    partial view ve componentler yapıldı. destinationmanager oluşturuldu

---
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,43 @@
+﻿
+
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class DestinationManager : IDestinationService
+    {
+        private readonly IDestinationDal _destinationDal;
+
+        public DestinationManager(IDestinationDal destinationDal)
+        {
+            _destinationDal = destinationDal;
+        }
+
+        public void TAdd(Destination t)
+        {
+            _destinationDal.Insert(t);
+        }
+
+        public void TDelete(Destination t)
+        {
+            _destinationDal.Delete(t);
+        }
+
+        public Destination TGetById(int id)
+        {
+           return _destinationDal.GetById(id);
+        }
+
+        public List<Destination> TGetList()
+        {
+            return _destinationDal.GetList();
+        }
+
+        public void TUpdate(Destination t)
+        {
+           _destinationDal.Update(t);
+        }
+    }
+}
