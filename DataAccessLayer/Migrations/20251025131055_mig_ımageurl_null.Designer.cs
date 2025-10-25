303bbedd75ae7ea0faac2cc7b8e615c35b17eeca
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-10-25 16:35:48 +0300

    kayıt olma işlemi tamamlandı.

--- DataAccessLayer/Migrations/20251025131055_mig_ımageurl_null.Designer.cs
+++ DataAccessLayer/Migrations/20251025131055_mig_ımageurl_null.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20251025131055_mig_ımageurl_null")]
+    partial class mig_ımageurl_null
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
