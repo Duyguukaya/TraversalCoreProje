8068febce268b9a93376d45728335d6b603b23f4
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-10-25 15:21:56 +0300

    kayıt ve giriş sayfaları oluşturuldu.

---
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class LoginController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
