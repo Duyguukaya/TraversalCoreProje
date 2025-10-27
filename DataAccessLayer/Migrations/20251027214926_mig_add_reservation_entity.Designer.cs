d78fc6ea0432134e2caf803aeba917e85c1a3dde
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-10-28 01:50:53 +0300

    rezervasyon kısmı yapıldı. Layoutta link verildi

--- DataAccessLayer/Migrations/20251027214926_mig_add_reservation_entity.Designer.cs
+++ DataAccessLayer/Migrations/20251027214926_mig_add_reservation_entity.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20251027214926_mig_add_reservation_entity")]
+    partial class mig_add_reservation_entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -460,13 +462,14 @@ namespace DataAccessLayer.Migrations
                         .HasColumnType("datetime2");
 
                     b.Property<string>("Status")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("ReservationId");
 
                     b.HasIndex("AppUserId");
 
-                    b.ToTable("Reservations");
+                    b.ToTable("Reservation");
                 });
 
             modelBuilder.Entity("EntityLayer.Concrete.SubAbout", b =>
