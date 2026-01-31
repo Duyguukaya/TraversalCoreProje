9fa6266c44c98008cf330f5ce57906c9e2a8b124
Duyguukaya <Duyguukaya@users.noreply.github.com>
2026-01-31 20:39:02 +0300

    mesaj gönderme işlemleri tamamlandı.

---
+++ TraversalCoreProje/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,48 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+        
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Traversal Rezervasyon", "traversaldeneme");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("Sayın Misafir", mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            BodyBuilder bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+
+           
+            smtpClient.Authenticate("traversaldeneme", "sdrrbbpdleoqxfhu");
+
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+            return View();
+        }
+    }
+}
