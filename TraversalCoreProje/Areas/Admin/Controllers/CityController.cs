1f68aa27acbeb979b023617c1ecf481c6dce959b
Duyguukaya <Duyguukaya@users.noreply.github.com>
2026-02-28 13:02:50 +0300

    Ajax işlemlerine başlandı.

---
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(cities);
+            return Json(jsonCity);
+        }
+
+
+        public static List<CityClass> cities = new List<CityClass>
+        {
+            new CityClass
+            {
+                CityId=1,
+                CityName="Üsküp",
+                CityCountry="Makedonya"
+            },
+            new CityClass
+            {
+                CityId=2,
+                CityName="Saraybosna",
+                CityCountry="Bosna Hersek"
+            },
+            new CityClass
+            {
+                CityId=3,
+                CityName="Roma",
+                CityCountry="İtalya"
+            }
+        };
+    }
+}
