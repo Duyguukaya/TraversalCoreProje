18663db9df8627114bf1d579a60b3d9898b5a983
Duyguukaya <Duyguukaya@users.noreply.github.com>
2026-02-28 14:38:01 +0300

    Ajax işlemleri tamamlandı.

--- EntityLayer/Concrete/Destination.cs
+++ EntityLayer/Concrete/Destination.cs
@@ -9,15 +9,15 @@ namespace EntityLayer.Concrete
         public string City { get; set; }
         public string DayNight { get; set; }
         public double Price { get; set; }
-        public string Image { get; set; }
-        public string Description { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
         public int Capacity { get; set; }
         public bool Satus { get; set; }
-        public string CoverImage { get; set; }
-        public string Details1 { get; set; }
-        public string Details2 { get; set; }
-        public string Image2 { get; set; }
-        public string Image3 { get; set; }
+        public string? CoverImage { get; set; }
+        public string? Details1 { get; set; }
+        public string? Details2 { get; set; }
+        public string? Image2 { get; set; }
+        public string? Image3 { get; set; }
 
         public List<Comment> Comments { get; set; }
         public List<Reservation> Reservations { get; set; }
--- TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TraversalCoreProje.Models;
 
@@ -7,6 +9,13 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
     [Area("Admin")]
     public class CityController : Controller
     {
+        private readonly IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,31 +24,68 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
 
         public IActionResult CityList()
         {
-            var jsonCity = JsonConvert.SerializeObject(cities);
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
             return Json(jsonCity);
         }
 
-
-        public static List<CityClass> cities = new List<CityClass>
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination destination)
         {
-            new CityClass
-            {
-                CityId=1,
-                CityName="Üsküp",
-                CityCountry="Makedonya"
-            },
-            new CityClass
-            {
-                CityId=2,
-                CityName="Saraybosna",
-                CityCountry="Bosna Hersek"
-            },
-            new CityClass
-            {
-                CityId=3,
-                CityName="Roma",
-                CityCountry="İtalya"
-            }
-        };
+            destination.Satus = true;
+            _destinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination);
+            return Json(destination);
+
+        }
+
+        public IActionResult GetById(int DestinationId)
+        {
+            var values = _destinationService.TGetById(DestinationId);
+            var jsonValues= JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+        }
+
+
+        public IActionResult DeleteCity(int id)
+        {
+            var values = _destinationService.TGetById(id);
+            _destinationService.TDelete(values);
+            return NoContent();
+        }
+
+
+        public IActionResult UpdateCity(Destination destination)
+        {
+            _destinationService.TUpdate(destination);
+            var valuesJson = JsonConvert.SerializeObject(destination);
+            return Json(valuesJson);
+        }
+
+
+
+
+
+
+        //public static List<CityClass> cities = new List<CityClass>
+        //{
+        //    new CityClass
+        //    {
+        //        CityId=1,
+        //        CityName="Üsküp",
+        //        CityCountry="Makedonya"
+        //    },
+        //    new CityClass
+        //    {
+        //        CityId=2,
+        //        CityName="Saraybosna",
+        //        CityCountry="Bosna Hersek"
+        //    },
+        //    new CityClass
+        //    {
+        //        CityId=3,
+        //        CityName="Roma",
+        //        CityCountry="İtalya"
+        //    }
+        //};
     }
 }
