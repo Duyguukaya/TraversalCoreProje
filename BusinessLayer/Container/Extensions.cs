af46665e13b150fa47375bb12b239da08d614a16
Duyguukaya <Duyguukaya@users.noreply.github.com>
2026-02-28 17:38:59 +0300

    contact_us entitysi eklendi işlemlerine başlandı.

--- BusinessLayer/Container/Extensions.cs
+++ BusinessLayer/Container/Extensions.cs
@@ -22,6 +22,9 @@ namespace BusinessLayer.Container
             services.AddScoped<IGuideDal, EfGuideDal>();
             services.AddScoped<IExcelService, ExcelManager>();
             services.AddScoped<IPdfService, PdfManager>();
+            services.AddScoped<IContactUsService, ContactUsManager>();
+            services.AddScoped<IContactUsDal, EfContactUsDal>();
+
 
         }
     }
