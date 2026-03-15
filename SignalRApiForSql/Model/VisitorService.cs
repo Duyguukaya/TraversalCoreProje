1032e9c6478fb34d4d6537ba72759df43a374ad7
Duyguukaya <Duyguukaya@users.noreply.github.com>
2026-03-15 05:40:27 +0300

    PostgreSqlden Sql servera geçildi.

---
+++ SignalRApiForSql/Model/VisitorService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using SignalRApiForSql.DAL;
+using SignalRApiForSql.Hubs;
+using System.Data;
+
+namespace SignalRApiForSql.Model
+{
+    public class VisitorService
+    {
+        private readonly Context _context;
+        private readonly IHubContext<VisitorHub> _hubContext;
+
+        public VisitorService(Context context, IHubContext<VisitorHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        public IQueryable<Visitor> GetList()
+        {
+            return _context.Visitors.AsQueryable();
+        }
+
+        public async Task SaveVisitor(Visitor visitor)
+        {
+            await _context.Visitors.AddAsync(visitor);
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("CallVisitList", "aaa");
+        }
+
+        public List<VisitorChart> GetVisitorChartList()
+        {
+            List<VisitorChart> visitorCharts = new List<VisitorChart>();
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = "select * from crosstab ('select VisitDate,City,CityVisitCount from Visitors Order By 1,2') as ct(VisitDate date,city1 int,city2 int,city3 int,city4 int,city5 int);";
+                command.CommandType = CommandType.Text;
+                _context.Database.OpenConnection();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        VisitorChart visitorChart = new VisitorChart();
+                        visitorChart.VisitDate = reader.GetDateTime(0).ToShortDateString();
+                        Enumerable.Range(1, 5).ToList().ForEach(x =>
+                        {
+                            visitorChart.Counts.Add(reader.GetInt32(x));
+                        });
+
+                        visitorCharts.Add(visitorChart);
+                    }
+                }
+                _context.Database.CloseConnection();
+                return visitorCharts;
+            }
+        }
+    }
+}
